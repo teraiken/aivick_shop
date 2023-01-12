734f36f547c687a0296228e706d4110e70844ad4
teraiken <k_terai@aivick.co.jp>
2023-01-12 17:26:00 +0900

    add：admin-user-index

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeSearch($query, $search)
+    {
+        if ($search !== null) {
+            $converted = mb_convert_kana($search, 's');
+            $searchSplited = preg_split('/[\s]+/', $converted);
+            foreach ($searchSplited as $value) {
+                $query->where('name', 'like', '%' .$value. '%');
+            }
+        }
+        return $query;
+    }
 }
