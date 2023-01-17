fdc5e2102dd6d8bb0d0e9cfe8b2f3cb2250e47ce
teraiken <k_terai@aivick.co.jp>
2023-01-17 13:24:37 +0900

    fix：user-product-index

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
 
     Route::resource('addresses', AddressController::class);
