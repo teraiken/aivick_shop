9c071b26a979f217387a3aa172027954480a27ad
teraiken <k_terai@aivick.co.jp>
2023-01-31 16:09:51 +0900

    fix：admin-product

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,49 +1,41 @@
 <x-admin-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            商品一覧
-        </h2>
+        商品一覧
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div class="text-right">
-                        <a href="{{ route('admin.products.create') }}" class="text-blue-500">新規登録</a>
+    <div class="text-right">
+        <a href="{{ route('admin.products.create') }}" class="text-blue-500">新規登録</a>
+    </div>
+
+    <section class="text-gray-600 body-font overflow-hidden">
+        <div class="container px-5 mx-auto">
+            <form class="mb-8 text-right" method="get" action="{{ route('admin.products.index') }}">
+                <x-search-form></x-search-form>
+            </form>
+
+            <div class="-my-8 divide-y-2 divide-gray-100">
+                @foreach($products as $product)
+                <div class="py-8 md:flex">
+                    <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                        <img src="{{ asset('storage/image/' . $product->image) }}">
+                        <span class="mt-1 text-gray-500 text-sm">{{ $product->created_at->format('Y/m/d') }}</span>
                     </div>
-
-                    <section class="text-gray-600 body-font overflow-hidden">
-                    <div class="container px-5 py-24 mx-auto">
-                        <form class="mb-8" method="get" action="{{ route('admin.products.index') }}">
-                            <x-search-form></x-search-form>
-                        </form>
-
-                        <div class="-my-8 divide-y-2 divide-gray-100">
-                        @foreach($products as $product)
-                        <div class="py-8 md:flex">
-                            <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                            <img src="{{ asset('storage/image/' . $product->image) }}">
-                            <span class="mt-1 text-gray-500 text-sm">{{ $product->created_at }}</span>
-                            </div>
-                            <div class="md:flex-grow relative pb-10 md:ml-2">
-                            <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{{ $product->name }}</h2>
-                            <p class="leading-relaxed">{{ $product->introduction }}</p>
-                            <a href="{{ route('admin.products.show', ['product' => $product->id]) }}" class="text-indigo-500 inline-flex items-center absolute bottom-0">詳細を見る
-                                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <div class="md:flex-grow relative pb-10 md:ml-2">
+                        <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{{ $product->name }}</h2>
+                        <p class="leading-relaxed">{{ $product->introduction }}</p>
+                        <a href="{{ route('admin.products.show', ['product' => $product->id]) }}"
+                            class="text-indigo-500 inline-flex items-center absolute bottom-0">詳細を見る
+                            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
+                                fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M5 12h14"></path>
                                 <path d="M12 5l7 7-7 7"></path>
-                                </svg>
-                            </a>
-                            </div>
-                        </div>
-                        @endforeach
-                        </div>
+                            </svg>
+                        </a>
                     </div>
-                    {{ $products->links() }}
-                    </section>
                 </div>
+                @endforeach
             </div>
         </div>
-    </div>
+        {{ $products->links() }}
+    </section>
 </x-admin-layout>
--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -1,99 +1,89 @@
 <x-admin-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            詳細画面
-        </h2>
+        詳細画面
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                <section class="text-gray-600 body-font relative">
-                <div class="container px-5 mx-auto">
-                    <div class="lg:w-1/2 md:w-2/3 mx-auto">
-                    <div class="flex flex-wrap -m-2">
-                        <div class="p-2 w-full">
+    <section class="text-gray-600 body-font relative">
+        <div class="container px-5 mx-auto">
+            <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                <div class="flex flex-wrap -m-2">
+                    <div class="p-2 w-full">
                         <div class="relative">
                             <x-input-label for="name">商品名</x-input-label>
                             <x-text-show>{{ $product->name }}</x-text-show>
                         </div>
-                        </div>
+                    </div>
 
-                        <div class="p-2 w-full">
+                    <div class="p-2 w-full">
                         <div class="relative">
                             <x-input-label>画像</x-input-label><br>
                             <img src="{{ asset('storage/image/' . $product->image) }}">
                         </div>
-                        </div>
-                        
-                        <div class="p-2 w-full">
+                    </div>
+
+                    <div class="p-2 w-full">
                         <div class="relative">
                             <x-input-label for="introduction">説明文</x-input-label>
                             <x-text-show>{{ $product->introduction }}</x-text-show>
                         </div>
-                        </div>
+                    </div>
 
-                        <div class="p-2 w-full">
+                    <div class="p-2 w-full">
                         <div class="relative">
                             <x-input-label for="price">税抜き価格</x-input-label>
                             <x-text-show>{{ $product->price }}</x-text-show>
                         </div>
-                        </div>
+                    </div>
 
-                        <div class="p-2 w-full">
+                    <div class="p-2 w-full">
                         <div class="relative">
                             <x-input-label for="stock">在庫数</x-input-label>
                             <x-text-show>{{ $product->stock }}</x-text-show>
                         </div>
-                        </div>
+                    </div>
 
-                        <div class="p-2 w-full">
+                    <div class="p-2 w-full">
                         <div class="relative">
                             <x-input-label>ステータス</x-input-label><br>
                             <x-text-show>{{ $productStatus->label() }}</x-text-show>
                         </div>
-                        </div>
-
-                        <div class="p-2 flex mx-auto">
-                            <form method="get" action="{{ route('admin.products.edit', ['product' => $product->id]) }}">
-                                <x-blue-button>
-                                    編集する
-                                </x-blue-button>
-                            </form>
-
-                            <x-danger-button class="ml-4"
-                                x-data=""
-                                x-on:click.prevent="$dispatch('open-modal', 'confirm-product-deletion')"
-                            >削除する</x-danger-button>
-                        </div>
-
-                        <x-modal name="confirm-product-deletion">
-                            <form method="post" action="{{ route('admin.products.destroy', ['product' => $product->id]) }}" class="p-6">
-                                @csrf
-                                @method('delete')
-
-                                <h2 class="text-lg font-medium text-gray-900">
-                                    本当に削除しますか？
-                                </h2>
-
-                                <div class="mt-6 flex justify-end">
-                                    <x-secondary-button x-on:click="$dispatch('close')">
-                                        キャンセル
-                                    </x-secondary-button>
-
-                                    <x-danger-button class="ml-3">
-                                        削除する
-                                    </x-danger-button>
-                                </div>
-                            </form>
-                        </x-modal>
                     </div>
+
+                    <div class="p-2 flex mx-auto">
+                        <form method="get" action="{{ route('admin.products.edit', ['product' => $product->id]) }}">
+                            <x-primary-button>
+                                編集する
+                            </x-primary-button>
+                        </form>
+
+                        <x-small-danger-button class="ml-4" x-data=""
+                            x-on:click.prevent="$dispatch('open-modal', 'confirm-product-deletion')">削除する
+                        </x-small-danger-button>
                     </div>
-                </div>
-                </section>
+
+                    <x-modal name="confirm-product-deletion">
+                        <form method="post" action="{{ route('admin.products.destroy', ['product' => $product->id]) }}"
+                            class="p-6">
+                            @csrf
+                            @method('delete')
+
+                            <h2 class="text-lg font-medium text-gray-900">
+                                本当に削除しますか？
+                            </h2>
+
+                            <div class="mt-6 flex justify-end">
+                                <x-secondary-button x-on:click="$dispatch('close')">
+                                    キャンセル
+                                </x-secondary-button>
+
+                                <x-danger-button class="ml-3">
+                                    削除する
+                                </x-danger-button>
+                            </div>
+                        </form>
+                    </x-modal>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </x-admin-layout>
