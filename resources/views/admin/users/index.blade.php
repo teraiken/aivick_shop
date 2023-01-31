a65cd2d83db16ef16ae912e821d27d7ecc9dd23b
teraiken <k_terai@aivick.co.jp>
2023-01-31 15:34:26 +0900

    fix：admin-user-index

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,44 +1,34 @@
 <x-admin-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            会員一覧
-        </h2>
+        会員一覧
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
-                    <form class="mb-8" method="get" action="{{ route('admin.users.index') }}">
-                        <x-search-form></x-search-form>
-                    </form>
-                    
-                    <table class="table-auto w-full text-left whitespace-no-wrap">
-                        <thead>
-                            <tr>
-                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">Id</th>
-                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">氏名</th>
-                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
-                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">登録日</th>
-                            </tr>
-                        </thead>
+    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+        <form class="mb-8 text-right" method="get" action="{{ route('admin.users.index') }}">
+            <x-search-form></x-search-form>
+        </form>
 
-                        <tbody>
-                        @foreach($users as $user)
-                            <tr>
-                                <td class="border-t-2 border-gray-200 px-4 py-3">{{ $user->id }}</td>
-                                <td class="border-t-2 border-gray-200 px-4 py-3">{{ $user->name }}</td>
-                                <td class="border-t-2 border-gray-200 px-4 py-3">{{ $user->email }}</td>
-                                <td class="border-t-2 border-gray-200 px-4 py-3">{{ $user->created_at }}</td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                    </div>
-                    {{ $users->links() }}
-                </div>
-            </div>
-        </div>
+        <table class="table-auto w-full text-left whitespace-no-wrap">
+            <thead>
+                <tr>
+                    <x-th>Id</x-th>
+                    <x-th>氏名</x-th>
+                    <x-th>メールアドレス</x-th>
+                    <x-th>登録日</x-th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($users as $user)
+                <tr>
+                    <x-td>{{ $user->id }}</x-td>
+                    <x-td>{{ $user->name }}</x-td>
+                    <x-td>{{ $user->email }}</x-td>
+                    <x-td>{{ $user->created_at->format('Y/m/d') }}</x-td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{ $users->links() }}
     </div>
 </x-admin-layout>
