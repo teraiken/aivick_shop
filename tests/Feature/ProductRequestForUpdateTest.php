4d24c1c210a16d3318adc88278f736ae52671007
teraiken <k_terai@aivick.co.jp>
2023-02-09 17:12:55 +0900

    fix：admin-product-test

--- tests/Feature/ProductRequestForUpdateTest.php
+++ tests/Feature/ProductRequestForUpdateTest.php
@@ -4,10 +4,10 @@ namespace Tests\Feature;
 
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
-use App\Http\Requests\ProductRequest;
+use App\Http\Requests\ProductRequestForUpdate;
 use Illuminate\Http\UploadedFile;
 
-class ProductRequestTest extends TestCase
+class ProductRequestForUpdateTest extends TestCase
 {
     /**
      * @dataProvider validationProvider
@@ -15,7 +15,7 @@ class ProductRequestTest extends TestCase
      */
     public function testValidation($inData, $outFail, $outMessage)
     {
-        $request = new ProductRequest();
+        $request = new ProductRequestForUpdate();
         $rules = $request->rules();
         $messages = $request->messages();
         $validator = Validator::make($inData, $rules, $messages);
@@ -31,11 +31,10 @@ class ProductRequestTest extends TestCase
             'success' => [
                 [
                     'name' => '弁当',
-                    'image' => UploadedFile::fake()->image('弁当.jpeg'),
                     'introduction' => '弁当です。弁当です。弁当です。弁当です。弁当です。',
                     'price' => 500,
                     'stock' => 10,
-                    'status' => 9,
+                    'start_date' => '2024-01-01',
                 ],
                 false,
                 [],
@@ -45,11 +44,25 @@ class ProductRequestTest extends TestCase
                 true,
                 [
                     'name' => ['名は必ず指定してください。'],
-                    'image' => ['画像は必ず指定してください。'],
                     'introduction' => ['説明文は必ず指定してください。'],
                     'price' => ['価格は必ず指定してください。'],
                     'stock' => ['在庫数は必ず指定してください。'],
-                    'status' => ['ステータスは必ず指定してください。'],
+                    'start_date' => ['開始日は必ず指定してください。'],
+                ],
+            ],
+            'period validation' => [
+                [
+                    'name' => '弁当',
+                    'image' => UploadedFile::fake()->image('弁当.jpeg'),
+                    'introduction' => '弁当です。弁当です。弁当です。弁当です。弁当です。',
+                    'price' => 500,
+                    'stock' => 10,
+                    'start_date' => '2023-01-01',
+                    'end_date' => '2022-01-01',
+                ],
+                true,
+                [
+                    'end_date' => ['終了日には、開始日以降の日付を指定してください。'],
                 ],
             ],
         ];
